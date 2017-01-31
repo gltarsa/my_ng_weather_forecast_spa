91a964a294b533114f2d58c0a1b79f857c8f46a9
Greg Tarsa <gltarsa@gmail.com>
2017-01-31 14:02:22 -0500

    Implement custom <weather-report> directive

--- app.js
+++ app.js
@@ -60,3 +60,20 @@ weatherSpa.controller('forecastController', ['$scope','$resource', '$routeParams
   }
 
 }]);
+
+weatherSpa.directive("weatherReport", function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/weatherReport.html',
+    replace: false,
+    scope: {
+      weatherObject: "=",
+      kelvinToStandard: "&",
+      dateToStandard: "&",
+      dateFormat: "@"
+    },
+    // link: function (scope, elems, attrs) {
+    // },
+    transclude: true
+  }
+})
